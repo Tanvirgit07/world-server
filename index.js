fa0da11d00cd871ed5015e4b8bd8847111b056e9
Tonmay1555 <ahmmedtonmay1555@gmail.com>
2024-04-29 04:02:55 +0600

    add information in database

--- index.js
+++ index.js
@@ -32,12 +32,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const spotCollection = client.db('spotDB').collection('spot');
+
+
+    app.post('/add',async(req,res) =>{
+        const newAdd = req.body;
+        console.log(newAdd)
+        const result = await spotCollection.insertOne(newAdd)
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
