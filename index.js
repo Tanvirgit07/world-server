6c5ce13c20ef28334ce04970cec6801fc6061ce8
Tonmay1555 <ahmmedtonmay1555@gmail.com>
2024-05-04 14:17:30 +0600

    added uri

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,7 +19,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 console.log(uri)
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -34,6 +34,81 @@ async function run() {
     await client.connect();
 
     const spotCollection = client.db('spotDB').collection('spot');
+    const countryCollection = client.db('spotDB').collection('country')
+
+
+    app.get('/country',async (req,res) =>{
+      const cursor = countryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/add',async(req,res) =>{
+      const cursor = spotCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/add/:id',async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const user = await spotCollection.findOne(query)
+      res.send(user)
+
+    });
+
+    app.get('/get-by-country/:countryName',async (req,res) => {
+      const name = req.params.countryName;
+      console.log(name)
+      // const query = {country_name : name}
+      const result = await spotCollection.find({country_name : name}).toArray()
+      res.send(result)
+    })
+
+
+    app.get('/myCart/:email', async(req,res) => {
+      const result = await spotCollection.find({email : req.params.email}).toArray()
+      res.send(result)
+    });
+
+
+    app.get('/add/:id',async (req,res) =>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const user = await spotCollection.findOne(query)
+      res.send(user);
+    })
+
+    app.put('/add/:id',async (req,res) =>{
+      const id = req.params.id;
+      const updateUser = req.body;
+      console.log(updateUser)
+      const filter = {_id : new ObjectId(id)};
+      const options = {upsert : true}
+      const updateData ={
+        $set : {
+          photo : updateUser.photo,
+          spot_name : updateUser.spot_name,
+          country_name : updateUser.country_name,
+          location : updateUser.location,
+          description : updateUser.description,
+          cost : updateUser.cost,
+          seasonality : updateUser.seasonality,
+          time : updateUser.time,
+          per_year : updateUser.per_year,
+        }
+      }
+      const result = await spotCollection.updateOne(filter,updateData,options);
+      res.send(result)
+    });
+
+    app.delete('/add/:id',async(req,res) =>{
+      const id = req.params.id;
+      console.log('id is',id)
+      const query = {_id : new ObjectId(id)}
+      const result = await spotCollection.deleteOne(query)
+      res.send(result);
+    })
 
 
     app.post('/add',async(req,res) =>{
